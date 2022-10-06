d86f00aed48e454bd887c9df45075d34b87bcddc
Dooplik <107807585+Dooplik@users.noreply.github.com>
2022-10-06 22:30:10 +0500

    Commit2

--- proj2/main.py
+++ proj2/main.py
@@ -1,11 +1,19 @@
-def bubble(n):
+def bubble(n, route):
     for i in range(len(n)):
         for j in range(len(n) - 1):
-            if n[j] > n[j + 1]:
-                n[j], n[j + 1] = n[j + 1], n[j]
+            if route == 1:
+                if n[j] > n[j + 1]:
+                    n[j], n[j + 1] = n[j + 1], n[j]
+            elif route == 0:
+                if n[j] < n[j + 1]:
+                    n[j], n[j + 1] = n[j + 1], n[j]
+            else:
+                print("ERROR")
     return n
 
 
 print("Введите n, а затем n чисел через enter")
 n = [int(input()) for i in range(int(input()))]
-print(bubble(n))
+print("Введите направление\n1 - По возрастанию\n0 - По убыванию")
+r = int(input())
+print(bubble(n, r))
